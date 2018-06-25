a4bd71acf9f5b099bedd5bb3d47d8852d8a271b8
Z M <zayneyre@gmail.com>
2018-06-25 01:01:09 -0700

    Complete Simon game, pass all specs, implement console screen clearing and timed interactivity

--- W2D1/lib/simon.rb
+++ W2D1/lib/simon.rb
@@ -4,38 +4,81 @@ class Simon
   attr_accessor :sequence_length, :game_over, :seq
 
   def initialize
-
+    @seq = []
+    @sequence_length = 1
+    @game_over = false
   end
 
   def play
+    clear
+    puts "Repeat after me!"
+    wait
 
+    until game_over
+      take_turn
+    end
+
+    game_over_message
+    reset_game
   end
 
   def take_turn
+    clear
+    show_sequence
 
+    require_sequence
+    @sequence_length += 1
+
+    round_success_message unless game_over
   end
 
   def show_sequence
+    clear
+    add_random_color
 
+    seq.each do |color|
+      puts color
+      wait(0.2)
+    end
+
+    wait(1)
+    clear
   end
 
   def require_sequence
-
+    sequence_length.times do |index|
+      unless gets.chomp == seq[index]
+        @game_over = true
+        break
+      end
+    end
   end
 
   def add_random_color
-
+    @seq << COLORS.sample
   end
 
   def round_success_message
-
+    clear
+    puts "Nice!"
+    wait
   end
 
   def game_over_message
-
+    puts "Not Nice!", "Game Over: Level #{sequence_length}"
   end
 
   def reset_game
+    @seq = []
+    @sequence_length = 1
+    @game_over = false
+  end
+  
+  def clear
+    system('clear')
+  end
 
+  def wait(seconds = 2)
+    sleep(seconds)
   end
 end
